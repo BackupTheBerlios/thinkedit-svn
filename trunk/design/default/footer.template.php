677b1fa68e3e1b50a80fed2f65bb525e9dd5b8e0
philippejadin <philippejadin@51d7789e-b304-0410-b37a-99b6bc0ed2a3>
2006-02-20 16:41:08 +0000

    new design (tmp) for Yapaka.be (this is a use case ;-) )

--- trunk/design/default/footer.template.php
+++ trunk/design/default/footer.template.php
@@ -1,2 +1,23 @@
-</body>
-</html>
+</td>
+													<td align="left" valign="top" width="168"></td>
+												</tr>
+											</table>
+										</td>
+									</tr>
+									<tr height="12">
+										<td align="left" valign="top" width="717"><span class="copyright">&copy;2006 YAPAKA</span></td>
+									</tr>
+								</table>
+							</td>
+							<td align="left" valign="top" width="15" background="<?php echo te_design(); ?>/sources/shadow_right.gif"></td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+			<tr>
+				<td align="center" valign="top" background="(EmptyReference!)"><img src="<?php echo te_design(); ?>/sources/shadow_bottom.gif" alt="" width="777" height="15" border="0"></td>
+			</tr>
+		</table>
+	</body>
+
+
