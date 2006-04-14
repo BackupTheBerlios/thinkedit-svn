cd80f6f7cae3b6f986227dfd3d2e261df6eca514
philippejadin <philippejadin@51d7789e-b304-0410-b37a-99b6bc0ed2a3>
2006-04-14 14:44:09 +0000

    updated yapaka template internal work

--- trunk/design/yapaka/content_footer.php
+++ trunk/design/yapaka/content_footer.php
@@ -4,17 +4,7 @@
 <td width="15"></td>
 <td width="168" valign="top">
 
-<?php if (isset($sidebar_image)): ?>
-<img src="<?php echo $sidebar_image->getThumbnail(array('w' => 170) ); ?>"/>
-<?php endif; ?>
-
-<br/>
-<br/>
-
-
-<?php include('relations.template.php'); ?>
-
-
+<?php include 'sidebar.php' ?>
 
 </td>
 
---
+++ trunk/design/yapaka/sidebar.php
@@ -0,0 +1,26 @@
+<?php if (isset($sidebar_image)): ?>
+<img src="<?php echo $sidebar_image->getThumbnail(array('w' => 170) ); ?>"/>
+<br/>
+<br/>
+<?php endif; ?>
+
+
+
+
+<?php include('relations.template.php'); ?>
+
+<br/>
+<br/>
+
+<?php if ($news = $node->getChildren(array('type'=>'news'))): ?>
+<?php foreach ($news as $actu): ?>
+
+<?php $actu_content = $actu->getContent(); ?>
+
+<a class="<?php echo te_get_section_name($actu) ?>_sub" href="<?php echo te_link($actu); ?>">
+<img src="<?php echo te_design() ?>/sources/fleche.gif">
+<?php echo te_short($actu_content->get('title'), 25); ?>
+</a>
+
+<?php endforeach; ?>
+<?php endif; ?>
