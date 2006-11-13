eb7c20e014f7b31f87978183e53e38b27227b5c8
philippejadin <philippejadin@51d7789e-b304-0410-b37a-99b6bc0ed2a3>
2006-11-13 17:07:41 +0000

    tinymce updated to latest

--- trunk/lib/tiny_mce/plugins/directionality/langs/en.js
+++ trunk/lib/tiny_mce/plugins/directionality/langs/en.js
@@ -1,6 +1,6 @@
 // UK lang variables
 
 tinyMCE.addToLang('',{
-directionality_ltr_desc : 'Direction left to right (Alt-.)',
-directionality_rtl_desc : 'Direction right to left (Alt-,)'
+directionality_ltr_desc : 'Direction left to right',
+directionality_rtl_desc : 'Direction right to left'
 });
