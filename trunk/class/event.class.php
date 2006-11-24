e9e387134e301d98eceb6a9d62280b9a8d76b5b0
philippejadin <philippejadin@51d7789e-b304-0410-b37a-99b6bc0ed2a3>
2006-11-24 11:52:41 +0000

    event class updated

--- trunk/class/event.class.php
+++ trunk/class/event.class.php
@@ -8,29 +8,48 @@ This is a work in progress
 */
 class event
 {
+	
+	var $events;
+	
+	/**
+	Register a $function to be called when an $event happens
+	
+	Example : 
+	$event->on('record_create', 'my_rss_builder');
+	Would call my_rss_builder() each time a record is created
+	
+	*/
+	function on($event, $function)
+	{
+		$this->events[$event][] = $function;
+	}
+	
+	/**
+	Notify the event object that something happened. 
+	You can add aditional parameters those will be added to the registered function called 
+	*/
+	function call($event)
+	{
 		
-		/**
-		Register a $function to be called when an $event happens
 		
-		Example : 
-		$event->register('record_create', 'my_rss_builder');
-		  Would call my_rss_builder() each time a record is created
+		// see if there are any funcitons or class method registered for this $event
 		
-		*/
-		function register($event, $function)
+		// call each function/class method for this $event using args
+		$args = func_get_args();
+		
+		call_user_func_array ($args);
+		
+		for($i = 1; $i < func_num_args(); $i++) 
 		{
-				
+			$string .= $glue;
+			$string .= func_get_arg($i);
 		}
 		
-		/**
-		Notify the event object that something happened. 
-		You can add aditional parameters those will be added to the registered function called 
-		*/
-		function call($event)
-		{
-				
-		}
+		return $string;
 		
-		
+	}
+}
+
+
 }
 ?>
